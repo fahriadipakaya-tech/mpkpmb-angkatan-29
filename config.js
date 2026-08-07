1526ef897fd978cac1bd31d4183d7002e4457d7d
fahriadipakaya-tech <fahriadipakaya@gmail.com>
2026-08-07 21:34:37 +0800

    Load MPK-PMB Dashboard 2.0 enhancements

--- config.js
+++ config.js
@@ -7,3 +7,12 @@ window.MPK_CONFIG = {
   ALLOW_DEMO_LOGIN: true,
   AUTO_SYNC_SECONDS: 30
 };
+
+window.addEventListener('load', () => {
+  if (document.querySelector('script[data-mpk-enhancements]')) return;
+  const s = document.createElement('script');
+  s.src = './enhancements.js?v=20260807-2126';
+  s.dataset.mpkEnhancements = '1';
+  s.defer = true;
+  document.body.appendChild(s);
+});
