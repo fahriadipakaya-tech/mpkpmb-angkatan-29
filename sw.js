1f4c0f3d7637dc5e64d85e5c33d200fae78397a8
fahriadipakaya-tech <fahriadipakaya@gmail.com>
2026-08-07 20:13:09 +0800

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='mpkpmb-xxix-v2';
+const ASSETS=['./','./index.html','./styles.css','./config.js','./roster.js','./data.js','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
