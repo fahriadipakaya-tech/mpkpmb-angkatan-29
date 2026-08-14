3afd92259b23ae3c4306e4196e5259fc8e5822ea
fahriadipakaya-tech <fahriadipakaya@gmail.com>
2026-08-14 20:20:40 +0800

    Bump coordinator planner cache

--- koordinator/sw.js
+++ koordinator/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mpk-koordinator-v1';
+const CACHE='mpk-koordinator-planner-v2';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','../icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
